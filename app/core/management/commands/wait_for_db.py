a1efc3a074d2131e2d289f8c1fcc7901bd472754
Andrey <druha.klimovets@mail.ru>
2023-06-22 16:07:13 +0300

    Lint fixes

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -1,15 +1,17 @@
 """
 Django command to wait for database to be available.
 """
+
 import time
 
 from psycopg2 import OperationalError as Psycopg2OpError
 from django.db.utils import OperationalError
 from django.core.management.base import BaseCommand
 
+
 class Command(BaseCommand):
     """Wait for db class"""
-    
+
     def handle(self, *args, **options):
         """ Db cheker. """
         self.stdout.write('Database is starting!')
