e491ab68027b32256e9a6d587f0c6e448cf2f716
Andrey <druha.klimovets@mail.ru>
2023-07-20 13:45:20 +0300

    Added delete ingredient view + test

--- app/recipe/tests/test_ingredients_api.py
+++ app/recipe/tests/test_ingredients_api.py
@@ -76,10 +76,21 @@ class PrivateIngredientsApiTest(TestCase):
         """Test updating an ingridient."""
         ingredient = Ingredient.objects.create(user=self.user, name='Cilantro')
 
-        payload = {'name':'Coriander'}
+        payload = {'name': 'Coriander'}
         url = detail_url(ingredient.id)
         res = self.client.patch(url, payload)
 
         self.assertEqual(res.status_code, status.HTTP_200_OK)
         ingredient.refresh_from_db()
         self.assertEqual(ingredient.name, payload['name'])
+
+    def test_delete_ingredient(self):
+        """Test deleting an ingredient."""
+        ingredient = Ingredient.objects.create(user=self.user, name='Lettuce')
+
+        url = detail_url(ingredient.id)
+        res = self.client.delete(url)
+
+        self.assertEqual(res.status_code, status.HTTP_204_NO_CONTENT)
+        ingredients = Ingredient.objects.filter(user=self.user)
+        self.assertFalse(ingredients.exists())
